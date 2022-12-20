53eb7c8ca0b0989a4f6b9a725304bb5c4ecc2eb6
Riina <laukkanen_riina@hotmail.com>
2022-12-20 14:18:41 +0200

    little fixes

--- database.sql
+++ database.sql
@@ -17,35 +17,36 @@ create table product (
 	price DECIMAL(6,2),
 	foreign key(cat_id) references category(cat_id)
 );
+-- customer-, order_ ja order_info-taulut ei käytössä, koska ei ole tilaussivua
 
-create table customer (
-	customer_id INTEGER primary key not null,
-	firstname varchar(50) not null,
-	lastname VARCHAR(50) not null,
-	address varchar(250) not null,
-	postcode varchar(10) not null,
-	city varchar(50) not null,
-	telnro varchar(20) not null,
-	email varchar(250) not null unique
-);
+-- create table customer (
+-- 	customer_id INTEGER primary key not null,
+-- 	firstname varchar(50) not null,
+-- 	lastname VARCHAR(50) not null,
+-- 	address varchar(250) not null,
+-- 	postcode varchar(10) not null,
+-- 	city varchar(50) not null,
+-- 	telnro varchar(20) not null,
+-- 	email varchar(250) not null unique
+-- );
 
-create table order_ (
-	order_id INTEGER primary key not null,
-	customer_id int not null, 
-	order_date DATETIME CURRENT_TIMESTAMP,
-	foreign key(customer_id) references customer(customer_id)
-	on delete restrict
-);
+-- create table order_ (
+-- 	order_id INTEGER primary key not null,
+-- 	customer_id int not null, 
+-- 	order_date DATETIME CURRENT_TIMESTAMP,
+-- 	foreign key(customer_id) references customer(customer_id)
+-- 	on delete restrict
+-- );
 
-create table order_info (
-	row_id INTEGER not null primary key,
- 	order_id INTEGER not null,
-	product_id INTEGER not null,
-	amount INTEGER not null,
-	foreign key(order_id) references order(order_id),
-	foreign key(product_id) references product(product_id)
-	on delete restrict
-);
+-- create table order_info (
+-- 	row_id INTEGER not null primary key,
+--  order_id INTEGER not null,
+-- 	product_id INTEGER not null,
+-- 	amount INTEGER not null,
+-- 	foreign key(order_id) references order(order_id),
+-- 	foreign key(product_id) references product(product_id)
+-- 	on delete restrict
+-- );
 insert into category (cat_name) values ('Matcha'), ('Green'), ('White'),('Rooibos');
 
  insert into product (tea_name,cat_id, price) values ('Matcha-jauhe', 1, 12), ('Matcha vanilja', 1, 5), ('Matcha latte, jauhe', 1, 16), ('Vihreä tyrnitee', 2, 4), ('Kissanpäivät', 2, 3), ('Paratiisi Sencha', 2, 3), ('Valkoinen helmi', 3, 4), ('Valkoinen Usva', 3, 3), ('Talven taika', 3, 3), ('Piparkakku Rooibos-pussitee', 4, 4), ('Appelsiini-Chili Rooibos', 4, 4), ('Luomu Rooibos', 4, 5);
